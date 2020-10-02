955939578eb21280dc75237bc96ec44d36ab351f
steven <github@stevenwaterman.uk>
2020-10-02 09:57:44 +0100

    layer locking, shift key, nav hands swap

--- keymap.c
+++ keymap.c
@@ -39,10 +39,23 @@
 
 enum custom_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
-  ST_MACRO_0,
-  ST_MACRO_1,
-  NAV_LAYER,
-  LOCK
+  LOCK,
+  LAYER_1,
+  LAYER_2,
+  LAYER_3,
+  LAYER_4,
+  LAYER_5,
+  LAYER_8,
+  UNSHIFT_HASH,
+  UNSHIFT_RABK,
+  UNSHIFT_BSLASH,
+  UNSHIFT_RCBR,
+  UNSHIFT_RBRACKET,
+  UNSHIFT_ENTER,
+  UNSHIFT_CUT,
+  UNSHIFT_COPY,
+  UNSHIFT_PASTE,
+  UNSHIFT_PROGRAMS
 };
 
 bool locked = false;
@@ -50,27 +63,29 @@ bool holding_layer = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
-    LOCK, LSFT(KC_QUOTE), KC_GRAVE, KC_F, KC_B, KC_QUES, MO(6), TO(7), KC_LABK, KC_K, KC_Q, KC_SLASH, KC_MINUS, KC_ASTR,
-    KC_TAB, KC_QUOTE, KC_H, KC_D, KC_P, KC_COMMA, MO(5), MO(5), KC_LCBR, KC_L, KC_U, KC_Y, KC_EQUAL, KC_PLUS,
-    LSFT_T(KC_ESCAPE),KC_A, KC_R, KC_S, KC_T, KC_DOT, KC_LPRN, KC_N, KC_E, KC_I, KC_O, KC_ENTER,
-    MO(1), KC_Z, KC_W, KC_G, KC_C, KC_SCOLON, MO(2), MO(2), KC_LBRACKET, KC_M, KC_V, KC_J, KC_X, MO(1),
+    LOCK, LSFT(KC_QUOTE), KC_GRAVE, KC_F, KC_B, KC_QUES, _, _, KC_LABK, KC_K, KC_Q, KC_SLASH, KC_MINUS, KC_ASTR,
+    KC_TAB, KC_QUOTE, KC_H, KC_D, KC_P, KC_COMMA, LAYER_5, LAYER_5, KC_LCBR, KC_L, KC_U, KC_Y, KC_EQUAL, KC_PLUS,
+    KC_ESCAPE,KC_A, KC_R, KC_S, KC_T, KC_DOT, KC_LPRN, KC_N, KC_E, KC_I, KC_O, KC_ENTER,
+    LAYER_1, KC_Z, KC_W, KC_G, KC_C, KC_SCOLON, LAYER_2, LAYER_2, KC_LBRACKET, KC_M, KC_V, KC_J, KC_X, LAYER_1,
     KC_LCTRL, KC_LGUI, KC_LALT, KC_DELETE, KC_BSPACE, KC_BSPACE, KC_HYPR, KC_MEH, KC_DLR, KC_PSCREEN,
-    DYN_MACRO_PLAY1,DYN_MACRO_PLAY2,DYN_REC_STOP, MO(8),
+    DYN_MACRO_PLAY1,DYN_MACRO_PLAY2,DYN_REC_STOP, LAYER_8,
     KC_MEDIA_PLAY_PAUSE,DYN_REC_START1,
-    KC_SPACE, NAV_LAYER, KC_MEDIA_NEXT_TRACK,DYN_REC_START2, NAV_LAYER, KC_SPACE
+    KC_SPACE, LAYER_3, KC_MEDIA_NEXT_TRACK,DYN_REC_START2, LAYER_3, KC_SPACE
   ),
 
+  // 
   [1] = LAYOUT_ergodox_pretty(
-    _, LSFT(KC_BSLASH),LSFT(KC_NONUS_BSLASH),LSFT(KC_F), LSFT(KC_B), KC_NONUS_HASH, _, _, KC_RABK, LSFT(KC_K), LSFT(KC_Q), KC_NONUS_BSLASH,KC_CIRC, KC_PERC,
-    LSFT(KC_TAB), LSFT(KC_2), LSFT(KC_H), LSFT(KC_D), LSFT(KC_P), KC_AMPR, _, _, KC_RCBR, LSFT(KC_L), LSFT(KC_U), LSFT(KC_Y), KC_UNDS, _,
-    _, LSFT(KC_A), LSFT(KC_R), LSFT(KC_S), LSFT(KC_T), KC_EXLM, KC_RPRN, LSFT(KC_N), LSFT(KC_E), LSFT(KC_I), LSFT(KC_O), LSFT(KC_ENTER),
-    _, LSFT(KC_Z), LSFT(KC_W), LSFT(KC_G), LSFT(KC_C), KC_COLN, _, _, KC_RBRACKET, LSFT(KC_M), LSFT(KC_V), LSFT(KC_J), LSFT(KC_X), _,
-    KC_PC_CUT, KC_PC_COPY, KC_PC_PASTE, LALT(LGUI(KC_0)),_, _, LALT(LGUI(KC_0)),_, LSFT(KC_3), KC_PAUSE,
+    _, KC_BSLASH, KC_NONUS_BSLASH,KC_F, KC_B, UNSHIFT_HASH, _, _, UNSHIFT_RABK, KC_K, KC_Q, UNSHIFT_BSLASH, KC_6, KC_5,
+    KC_TAB, KC_2, KC_H, KC_D, KC_P, KC_7, _, _, UNSHIFT_RCBR, KC_L, KC_U, KC_Y, KC_MINUS, _,
+    UNSHIFT_ENTER, KC_A, KC_R, KC_S, KC_T, KC_1, KC_0, KC_N, KC_E, KC_I, KC_O, KC_ENTER,
+    KC_NO, KC_Z, KC_W, KC_G, KC_C, KC_SCOLON, _, _, UNSHIFT_RBRACKET, KC_M, KC_V, KC_J, KC_X, _,
+    UNSHIFT_CUT, UNSHIFT_COPY, UNSHIFT_PASTE, UNSHIFT_PROGRAMS, _, _, UNSHIFT_PROGRAMS,_, KC_3, KC_PAUSE,
     _, _, _, _,
     _, _,
-    KC_ENTER, _, _, _, _, KC_ENTER
+    _, _, _, _, _, _
   ),
 
+  // Symbol layer
   [2] = LAYOUT_ergodox_pretty(
     _, KC_NO, KC_PERC, KC_9, KC_8, KC_RPRN, _, _, KC_RPRN, KC_8, KC_9, KC_PERC, KC_NO, KC_NO,
     _, KC_7, KC_6, KC_5, KC_4, KC_LPRN, _, _, KC_LPRN, KC_4, KC_5, KC_6, KC_7, KC_NO,
@@ -82,19 +97,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _, _, _, _, _, _
   ),
   
+  // Nav layer
   [3] = LAYOUT_ergodox_pretty(
-    _, KC_PC_UNDO, KC_PC_CUT, KC_PC_COPY, KC_PC_PASTE, KC_NO, _, _, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    _, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, _, _, KC_NO, KC_MS_WH_DOWN, KC_MS_UP, KC_MS_WH_UP, KC_NO, _,
-    _, KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_MS_WH_LEFT, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_RIGHT, _,
-    KC_LSFT, KC_NO, KC_V, KC_HOME, KC_END, KC_NO,_, _, KC_MS_BTN4, KC_MS_BTN5, KC_NO, KC_NO, KC_NO, KC_LSFT,
-    _, _, _, _, _, KC_MS_BTN3, KC_NO, KC_NO, KC_NO, KC_RCTRL,
+    _, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _, _, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    _, KC_NO, KC_MS_WH_DOWN, KC_MS_UP, KC_MS_WH_UP, KC_NO, _, _, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, _,
+    _, KC_MS_WH_LEFT, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_RIGHT, KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, _,
+    KC_LSFT, KC_NO, KC_NO, KC_NO, KC_END, KC_NO,_, _, KC_NO, KC_HOME, KC_NO, KC_END, KC_NO, KC_LSFT,
+    _, _, _, _, _, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     _, _, _, _,
     _, _,
-    KC_MS_BTN1, KC_MS_BTN2, TO(0), TO(0), KC_MS_BTN2, KC_MS_BTN1
+    KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3, KC_MS_BTN3, KC_MS_BTN2, KC_MS_BTN1
   ),
 
+  // F keys
   [4] = LAYOUT_ergodox_pretty(
-    _, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, TO(0), _, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    _, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _, _, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     _, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, _, _, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _,
     _, KC_F11, KC_F12, KC_F13, KC_F14, KC_F15, KC_INSERT, KC_NUMLOCK, KC_SCROLLLOCK, KC_CAPSLOCK, KC_RALT, _,
     OSM(MOD_LSFT), KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, _, _, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, OSM(MOD_LSFT),
@@ -104,6 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO, _, _, _, _, KC_NO
   ),
   
+  // i3
   [5] = LAYOUT_ergodox_pretty(
     _, _, _, LALT(LGUI(KC_F)),LALT(LGUI(KC_B)),_, _, _, _, LALT(LGUI(KC_K)),LALT(LGUI(KC_Q)),_, _, LALT(LGUI(KC_F9)),
     _, _, LALT(LGUI(KC_H)),LALT(LGUI(KC_D)),LALT(LGUI(KC_P)),LALT(LGUI(KC_F1)),_, _, LALT(LGUI(KC_F1)),LALT(LGUI(KC_L)),LALT(LGUI(KC_UP)),LALT(LGUI(KC_Y)),LALT(KC_F12), _,
@@ -113,28 +131,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _, _, _, _,
     _, _,
     LALT(LGUI(KC_F3)),_, _, _, _, LALT(LGUI(KC_F3))
-  ),
-  
-  [6] = LAYOUT_ergodox_pretty(
-    _, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _, _, ST_MACRO_1, KC_NO, KC_NO, KC_NO, KC_NO, TO(0),
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _, _, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MEDIA_PREV_TRACK,KC_MEDIA_PLAY_PAUSE,KC_MEDIA_NEXT_TRACK,KC_NO, KC_NO,
-    _, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _, _, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, _, KC_NO, KC_NO,
-    KC_NO, KC_NO,
-    KC_NO, _, KC_NO, KC_NO, _, KC_NO
-  ),
-  
-  [7] = LAYOUT_ergodox_pretty(
-    KC_F1, KC_F2, KC_F3, _, _, _, _, _, _, _, _, _, _, _,
-    _, _, _, _, _, _, _, _, _, _, _, _, _, _,
-    _, _, _, _, _, _, _, _, _, _, _, _,
-    _, _, _, _, _, _, _, _, _, _, _, _, _, _,
-    _, _, _, _, _, _, _, _, _, TO(0),
-    _, _, _, _,
-    _, _,
-    _, _, _, _, _, TO(0)
   ),
   
   [8] = LAYOUT_ergodox_pretty(
@@ -149,34 +145,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+bool layer(keyrecord_t *record, uint8_t layer) {
+  if (record->event.pressed) {
+    holding_layer = true;
+    layer_on(layer);
+  } else {
+    holding_layer = false;
+    if (locked) {
+      locked = false;
+    } else {
+      layer_off(layer);
+    }
+  }
+  return false;
+}
+
+bool unshifted(keyrecord_t *record, uint16_t keycode) {
+  if (record->event.pressed) {
+    unregister_code(KC_LSFT);
+    tap_code16(keycode);
+    register_code(KC_LSFT);
+  }
+  return false;
+}
+
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case ST_MACRO_0:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LALT(SS_LGUI(SS_TAP(X_ENTER))));
-      }
-      break;
-
-    case ST_MACRO_1:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LALT(SS_TAP(X_0)) SS_DELAY(100) SS_TAP(X_F) SS_DELAY(100) SS_TAP(X_I) SS_DELAY(100) SS_TAP(X_R) SS_DELAY(100) SS_TAP(X_ENTER));
-      }
-      break;
-
-    case NAV_LAYER:
-      if (record->event.pressed) {
-        holding_layer = true;
-        layer_on(3);
-      } else {
-        holding_layer = false;
-        if (locked) {
-          locked = false;
-        } else {
-          layer_off(3);
-        }
-      }
-      return false;
-      break;
+    case LAYER_1:
+      return layer(record, 1);
+    case LAYER_2:
+      return layer(record, 2);
+    case LAYER_3:
+      return layer(record, 3);
+    case LAYER_4:
+      return layer(record, 4);
+    case LAYER_5:
+      return layer(record, 5);
+    case LAYER_8:
+      return layer(record, 8);
 
     case LOCK:
       if (record->event.pressed) {
@@ -193,6 +200,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
       }
       break;
+
+    case UNSHIFT_HASH:
+      return unshifted(record, KC_HASH);
+    case UNSHIFT_RABK:
+      return unshifted(record, KC_RABK);
+    case UNSHIFT_BSLASH:
+      return unshifted(record, KC_BSLASH);
+    case UNSHIFT_RCBR:
+      return unshifted(record, KC_RCBR);
+    case UNSHIFT_RBRACKET:
+      return unshifted(record, KC_RBRACKET);
+    case UNSHIFT_ENTER:
+      return unshifted(record, KC_ENTER);
+    case UNSHIFT_CUT:
+      return unshifted(record, KC_PC_CUT);
+    case UNSHIFT_COPY:
+      return unshifted(record, KC_PC_COPY);
+    case UNSHIFT_PASTE:
+      return unshifted(record, KC_PC_PASTE);
+    case UNSHIFT_PROGRAMS:
+      return unshifted(record, LALT(LGUI(KC_0)));
   }
   return true;
 }
@@ -209,6 +237,12 @@ uint32_t layer_state_set_user(uint32_t state) {
   uint8_t msg[RAW_EPSIZE];
   sprintf((char *)msg, "LAYER:%u", layer);
   raw_hid_send(msg, RAW_EPSIZE);
+
+  if (layer == 1) {
+    register_code(KC_LSFT);
+  } else {
+    unregister_code(KC_LSFT);
+  }
 
   switch (layer) {
     case 1:
